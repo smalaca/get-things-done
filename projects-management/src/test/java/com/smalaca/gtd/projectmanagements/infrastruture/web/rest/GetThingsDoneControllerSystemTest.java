ff26bd9e737a2ce68b26296fbd780c63e0e47889
smalaca <ab13.krakow@gmail.com>
2021-11-05 14:31:25 +0100

    TASK 1 - add basic authentication

--- projects-management/src/test/java/com/smalaca/gtd/projectmanagements/infrastruture/web/rest/GetThingsDoneControllerSystemTest.java
+++ projects-management/src/test/java/com/smalaca/gtd/projectmanagements/infrastruture/web/rest/GetThingsDoneControllerSystemTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser("USER")
 @Tag("SystemTest")
 class GetThingsDoneControllerSystemTest {
     @Autowired
