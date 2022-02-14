39a662c8052df2f68671c781a47f0959717ba41b
smalaca <ab13.krakow@gmail.com>
2022-02-14 13:04:05 +0100

    REFACT - make ValidationFieldErrorDto final due to private constructor

--- projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/rest/validation/ValidationFieldErrorDto.java
+++ projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/rest/validation/ValidationFieldErrorDto.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Getter
-public class ValidationFieldErrorDto {
+public final class ValidationFieldErrorDto {
     private final List<String> fields;
     private final String message;
 
