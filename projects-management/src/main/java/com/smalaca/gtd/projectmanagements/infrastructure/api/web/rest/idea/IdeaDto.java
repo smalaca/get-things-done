4297c1f881f9bfb43a02419fae9168d87612c664
smalaca <ab13.krakow@gmail.com>
2022-02-14 11:38:37 +0100

    TASK-6 - add AtLeastOneNotEmpty constrain and validator to validate IdeaDto

--- projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/rest/idea/IdeaDto.java
+++ projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/rest/idea/IdeaDto.java
@@ -1,9 +1,16 @@
 package com.smalaca.gtd.projectmanagements.infrastructure.api.web.rest.idea;
 
 import com.smalaca.gtd.projectmanagements.domain.idea.CreateIdeaCommand;
+import com.smalaca.gtd.projectmanagements.infrastructure.validation.constraints.AtLeastOneNotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@AtLeastOneNotEmpty(
+        fields = {"title", "description"},
+        message = "Title or description cannot be empty."
+)
+@Getter
 class IdeaDto {
     private final String title;
     private final String description;
---
+++ projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/validation/constraints/AtLeastOneNotEmpty.java
@@ -0,0 +1,23 @@
+package com.smalaca.gtd.projectmanagements.infrastructure.validation.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({TYPE, ANNOTATION_TYPE})
+@Constraint(validatedBy = AtLeastOneNotEmptyValidator.class)
+public @interface AtLeastOneNotEmpty {
+    String message() default "At least one of the fields must be set";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String[] fields();
+}
