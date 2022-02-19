980c4fa36414206347454736391bf7004fb58d59
smalaca <ab13.krakow@gmail.com>
2022-02-19 21:26:44 +0100

    TASK-11 - use JDBC authentication instead of in memory

--- projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/security/WebSecurityConfiguration.java
+++ projects-management/src/main/java/com/smalaca/gtd/projectmanagements/infrastructure/api/web/security/WebSecurityConfiguration.java
@@ -6,8 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
+
+    WebSecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -23,9 +31,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 }
