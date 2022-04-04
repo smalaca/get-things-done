26526bf3c2e32052109c8c12cd2ca2cc46279b45
smalaca <ab13.krakow@gmail.com>
2022-04-04 13:29:04 +0200

    REFACT - use UserRepository instead of test class to save users

--- projects-management/src/test/java/com/smalaca/gtd/usermanagement/persistence/given/GivenUserManagementTestConfiguration.java
+++ projects-management/src/test/java/com/smalaca/gtd/usermanagement/persistence/given/GivenUserManagementTestConfiguration.java
@@ -1,5 +1,6 @@
 package com.smalaca.gtd.usermanagement.persistence.given;
 
+import com.smalaca.gtd.usermanagement.persistence.user.UserRepository;
 import com.smalaca.gtd.usermanagement.persistence.user.UserTestRepository;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -9,12 +10,12 @@ import org.springframework.context.annotation.Scope;
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
 @TestConfiguration
-@Import(UserTestRepository.class)
+@Import({UserRepository.class, UserTestRepository.class})
 public class GivenUserManagementTestConfiguration {
 
     @Bean
     @Scope(value = SCOPE_PROTOTYPE)
-    public GivenUsers givenUsers(UserTestRepository userTestRepository) {
-        return new GivenUsers(userTestRepository);
+    public GivenUsers givenUsers(UserRepository userRepository, UserTestRepository userTestRepository) {
+        return new GivenUsers(userRepository, userTestRepository);
     }
 }
