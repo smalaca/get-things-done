f7156f9d6321a5d58a6fc1812e8ec2f33d5b0b49
smalaca <ab13.krakow@gmail.com>
2022-04-08 21:09:37 +0200

    REFACT - add equals and hash code contract to ShareIdeaCommand

--- projects-management/src/main/java/com/smalaca/gtd/projectmanagement/application/idea/ShareIdeaCommand.java
+++ projects-management/src/main/java/com/smalaca/gtd/projectmanagement/application/idea/ShareIdeaCommand.java
@@ -3,12 +3,16 @@ package com.smalaca.gtd.projectmanagement.application.idea;
 import com.smalaca.gtd.projectmanagement.domain.author.AuthorId;
 import com.smalaca.gtd.projectmanagement.domain.collaborator.CollaboratorId;
 import com.smalaca.gtd.projectmanagement.domain.idea.IdeaId;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor
-public class ShareIdeaCommand {
+@EqualsAndHashCode
+@SuppressFBWarnings("RCN_REDUNDANT_NULLCHECK_OF_NONNULL_VALUE")
+public final class ShareIdeaCommand {
     private final UUID authorId;
     private final UUID ideaId;
     private final UUID collaboratorId;
